46ecb9e97b9e771b4a1c695ad0f1dc78e6e8d67a
Burak Şen <burak.sen@tum.de>
2024-04-30 01:46:54 +0200

    Read input file from arguments

--- 2015/1/python/main.py
+++ 2015/1/python/main.py
@@ -1,2 +1,11 @@
 import os
 import sys
+
+if __name__ == '__main__':
+    args = sys.argv
+
+    if len(args) < 2:
+        print('Usage: python main.py <file>')
+        sys.exit(1)
+
+    file = args[1]

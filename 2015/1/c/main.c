42cd0b635148dbb9f243a60f895c914260ed8976
Burak Şen <burak.sen@tum.de>
2024-04-30 01:53:55 +0200

    Change the wording

--- 2015/1/c/main.c
+++ 2015/1/c/main.c
@@ -5,7 +5,7 @@ int main(int argc, char **argv)
 {
     if (argc < 2)
     {
-        printf("Usage: %s <number>\n", argv[0]);
+        printf("Usage: %s <input_file>\n", argv[0]);
         return EXIT_FAILURE;
     }
 

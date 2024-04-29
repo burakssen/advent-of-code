bd15f5eec243288adba0d2c4771d8679b53a58d3
Burak Şen <burak.sen@tum.de>
2024-04-30 01:50:04 +0200

    Get the filename and check if file can be open

--- 2015/1/c/main.c
+++ 2015/1/c/main.c
@@ -9,5 +9,16 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
     }
 
+    char filename[20];
+    sprintf(filename, "%s", argv[1]);
+
+    FILE *file = fopen(filename, "r");
+
+    if (file == NULL)
+    {
+        printf("Error: Could not open file %s\n", filename);
+        return EXIT_FAILURE;
+    }
+
     return EXIT_SUCCESS;
 }

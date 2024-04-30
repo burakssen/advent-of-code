556ed85fb4653ac455cde168a45a52607e6b38b1
Burak Şen <burak.sen@tum.de>
2024-04-30 19:26:23 +0200

    Loop lines and characters and count calculate the floor value

--- 2015/1/rust/src/main.rs
+++ 2015/1/rust/src/main.rs
@@ -14,4 +14,19 @@ fn main() {
     let contents = std::fs::read_to_string(input_file)
         .expect("Something went wrong reading the file");
 
+    let mut count = 0;
+    // loop through the lines
+    for line in contents.lines() {
+        // loop through the characters
+        for c in line.chars() {
+            if c == '(' {
+                count += 1;
+            }
+            else if c == ')' {
+                count -= 1;
+            }
+        
+        }
+    }
+    println!("{}", count);
 }

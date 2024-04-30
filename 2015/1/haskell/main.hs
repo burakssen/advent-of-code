843ce5325b578866a479dc8454b25592423a15a4
Burak Şen <burak.sen@tum.de>
2024-04-30 18:57:55 +0200

    Calculate the floor value and return it

--- 2015/1/haskell/main.hs
+++ 2015/1/haskell/main.hs
@@ -13,3 +13,11 @@ processFile filename = do
   let floorNumber = calculateFloor (filter (`elem` "()") contents)
   print floorNumber
 
+calculateFloor :: String -> Int
+calculateFloor input = go input 0
+  where
+    go [] floor = floor
+    go (x : xs) floor
+      | x == '(' = go xs (floor + 1)
+      | x == ')' = go xs (floor - 1)
+      | otherwise = go xs floor

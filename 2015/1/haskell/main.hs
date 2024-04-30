dec0daec3883053ab6b8de2aaf9c12d06dd00ae5
Burak Şen <burak.sen@tum.de>
2024-04-30 18:56:49 +0200

    Get command line arguments and call the processFile function

---
+++ 2015/1/haskell/main.hs
@@ -0,0 +1,8 @@
+import System.Environment (getArgs)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    [filename] -> processFile filename
+    _ -> putStrLn "Usage: main <input_file>"

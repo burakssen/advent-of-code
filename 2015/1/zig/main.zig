85ff80cf8ac432296727d98b43daad43f4805253
Burak Şen <burak.sen@tum.de>
2024-04-30 19:32:03 +0200

    Open file

--- 2015/1/zig/main.zig
+++ 2015/1/zig/main.zig
@@ -12,4 +12,10 @@ pub fn main() !void {
         return;
     }
 
+    const file_path = args[1];
+    const file = try std.fs.cwd().openFile(file_path, .{});
+    defer file.close();
+
+    var buffered_reader = std.io.bufferedReader(file.reader());
+    var reader = buffered_reader.reader();
 }

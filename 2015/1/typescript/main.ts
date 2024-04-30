e9d1e87578dd03f4a10ccf90dac88afa128d8a38
Burak Şen <burak.sen@tum.de>
2024-04-30 19:19:37 +0200

    Check file exist

--- 2015/1/typescript/main.ts
+++ 2015/1/typescript/main.ts
@@ -8,6 +8,14 @@ function main() {
     console.log("Usage: node main.js <input_file>");
     process.exit(1);
   }
+
+  const filename: string = args[1];
+
+  if (!existsSync(filename)) {
+    console.log(`File not found: ${filename}`);
+    process.exit(1);
+  }
+
 }
 
 main();

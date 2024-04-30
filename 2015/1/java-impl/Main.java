0310ac4636aee970c237ea486f48f9fb618f719e
Burak Şen <burak.sen@tum.de>
2024-04-30 19:05:45 +0200

    Get command line arguments

---
+++ 2015/1/java-impl/Main.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Get command line arguments
+        String[] arguments = args;
+
+        if (arguments.length < 1) {
+            System.out.println("Usage: java Main <input_file>");
+            return;
+        }
+        
+    }
+}

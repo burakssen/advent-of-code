950886cf2ee12b0ee74747a575f42287b73fefe8
Burak Şen <burak.sen@tum.de>
2024-04-30 18:54:50 +0200

    Loop each line and character and count the floors

--- 2015/1/go/main.go
+++ 2015/1/go/main.go
@@ -11,10 +11,31 @@ func main() {
 		fmt.Println("Usage: go run main.go <input_file>")
 		return
 	}
+
 	file, err := os.Open(args[0])
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
+	// loop line by line
+	count := 0
+	for {
+		var line string
+		_, err := fmt.Fscanln(file, &line)
+		if err != nil {
+			break
+		}
+
+		// loop char by char
+		for _, c := range line {
+			if c == '(' {
+				count++
+			} else if c == ')' {
+				count--
+			}
+		}
+	}
+
+	fmt.Println(count)
 }

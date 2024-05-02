223d6a91250f5dd30cb76ead31f60f92994c717d
Burak Şen <burak.sen@tum.de>
2024-05-02 22:07:19 +0200

    Typescript implementation update for day1 part2

--- 2015/1/typescript/main.ts
+++ 2015/1/typescript/main.ts
@@ -20,22 +20,29 @@ function main() {
 
   const lines: string[] = data.split("\n");
   // loop each character
+  var floor: number = 0;
   var count: number = 0;
+  var basement: number = 0;
 
   for (let i = 0; i < lines.length; i++) {
     const line: string = lines[i];
     for (let j = 0; j < line.length; j++) {
       const c: string = line[j];
       if (c === "(") {
-        count++;
+        floor++;
+      } else if (c === ")") {
+        floor--;
       }
-      if (c === ")") {
-        count--;
+
+      count++;
+      if (floor === -1 && basement === 0) {
+        basement = count;
       }
     }
   }
 
-  console.log(count);
+  console.log(`Floor: ${floor}`);
+  console.log(`Basement: ${basement}`);
 }
 
 main();

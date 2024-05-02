19177ee81fd957a257e87604f2d449015e250c35
Burak Şen <burak.sen@tum.de>
2024-05-02 21:54:37 +0200

    Haskell implementation update for day1 part2

--- 2015/1/haskell/main.hs
+++ 2015/1/haskell/main.hs
@@ -10,14 +10,16 @@ main = do
 processFile :: FilePath -> IO ()
 processFile filename = do
   contents <- readFile filename
-  let floorNumber = calculateFloor (filter (`elem` "()") contents)
-  print floorNumber
+  let (floorNumber, basement) = calculateFloor (filter (`elem` "()") contents)
+  putStrLn $ "Floor: " ++ show floorNumber
+  putStrLn $ "Basement: " ++ show basement
 
-calculateFloor :: String -> Int
-calculateFloor input = go input 0
+calculateFloor :: String -> (Int, Int)
+calculateFloor input = go input (0, 0)
   where
-    go [] floor = floor
-    go (x : xs) floor
-      | x == '(' = go xs (floor + 1)
-      | x == ')' = go xs (floor - 1)
-      | otherwise = go xs floor
+    go [] (floor, basement) = (floor, basement)
+    go (x : xs) (floor, basement)
+      | floor == -1 && basement == 0 = go xs (floor - 1, length input - length xs - 1)
+      | x == '(' = go xs (floor + 1, basement)
+      | x == ')' = go xs (floor - 1, basement)
+      | otherwise = go xs (floor, basement)

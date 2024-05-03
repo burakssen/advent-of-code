60921c3d7f375413840a7a529af6725eab6b3366
Burak Şen <burak.sen@tum.de>
2024-05-03 23:05:35 +0200

    Rust implementation update for day2 part2

--- 2015/2/rust/src/main.rs
+++ 2015/2/rust/src/main.rs
@@ -12,7 +12,8 @@ fn main() {
     // split the input into lines
     let lines: Vec<&str> = input.lines().collect();
 
-    let mut total = 0;
+    let mut paper_size = 0;
+    let mut ribbon_length = 0;
     for line in lines {
         // split line into words by x
         let words: Vec<&str> = line.split('x').collect();
@@ -20,13 +21,15 @@ fn main() {
         let mut sides: Vec<i32> = words.iter().map(|x| x.parse().unwrap()).collect();
         // sort sides in ascending order
         sides.sort();
+
+        ribbon_length+=2*sides[0]+2*sides[1]+sides[0]*sides[1]*sides[2];
+        
         // calculate the area of the smallest side
         let area = sides[0] * sides[1];
         // calculate the total area
-        let total_area = 2 * sides[0] * sides[1] + 2 * sides[1] * sides[2] + 2 * sides[2] * sides[0];
-
-        total += total_area + area;
+        paper_size += 2 * area + 2 * sides[1] * sides[2] + 2 * sides[2] * sides[0] + area;
     }
 
-    println!("{}", total);
+    println!("Paper Size: {}", paper_size);
+    println!("Ribbon Length: {}", ribbon_length);
 }

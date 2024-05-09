ffe87dae47ce3e4e9b50bb473a220d375f2356e1
Burak Şen <burak.sen@tum.de>
2024-05-09 23:21:19 +0200

    Fix message

--- 2015/6/rust/src/part2/main.rs
+++ 2015/6/rust/src/part2/main.rs
@@ -91,7 +91,7 @@ fn main() {
     // get command line arguments
     let args: Vec<String> = std::env::args().collect();
     if args.len() != 2 {
-        println!("Usage: cargo run --bin part1 <input.txt>");
+        println!("Usage: cargo run --bin part2 <input.txt>");
         return;
     }
 
